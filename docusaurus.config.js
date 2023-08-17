51655c983c10c504ab846e953dfd62462ef61a53
Gene Kuo <igene@igene.tw>
2023-08-17 21:29:09 +0900

    Add Favicon and Social Card (#16)
    This commits add favicon and social card to website.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -62,7 +62,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: 'img/docusaurus-social-card.jpg',
+      image: 'img/cntug-logo-wide.png',
       navbar: {
         title: 'Cloud Native Taiwan User Group',
         logo: {
